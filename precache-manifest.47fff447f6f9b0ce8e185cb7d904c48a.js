2f566c22ca8d70f1172d95fa04b5c2e53f5f1e46
alexander-mikanovich <70010845+alexander-mikanovich@users.noreply.github.com>
2020-08-21 16:47:06 +0300

    Updates

---
+++ precache-manifest.47fff447f6f9b0ce8e185cb7d904c48a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8943624557d3b977072e520657fd8b5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "467d7eec68bd18e2a67b",
+    "url": "/my-app/static/css/main.c143e9be.chunk.css"
+  },
+  {
+    "revision": "315227eb87a582a96b7d",
+    "url": "/my-app/static/js/2.78ce79f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.78ce79f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467d7eec68bd18e2a67b",
+    "url": "/my-app/static/js/main.9eeb8f5d.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
